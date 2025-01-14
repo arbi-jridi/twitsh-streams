02aec3438b8b80febc13f38ad991cd41b12ac52e
arbi1990 <arbi.jridi@gmail.com>
2025-01-14 11:06:27 +0100

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA0Rtjhgl9qefwEo7Gr3MyW_xebXwQJV7E",
+    authDomain: "twitsh-2bfa9.firebaseapp.com",
+    projectId: "twitsh-2bfa9",
+    storageBucket: "twitsh-2bfa9.appspot.com",
+    appId: "1:666039515619:web:4bc3ef1e2c682c7c0b48d6"
+  }
+};
